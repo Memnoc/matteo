912203aca9004eaf09180c9d035bb798f23e1842
Memnoc <smartdroidesign@gmail.com>
2023-09-12 19:24:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+console.log(`
+Matteo Stara - Software Engineer
+
+
+Passionate about automations, CLI tools, and open source.
+I dedicate my time to learn new things and share my knowledge with others.
+At work, I'm a sytstem architect and a full-stack developer. I write tools for developers, and strive to make their life easier.
+
+Helping people throuhg technology is my mission.
+
+I hope I can help you too. <3
+
+You can find me on Twitter: 🐦
+You can find me on Instagram: 📸
+You can find me on Facebook: 👤
+
+`);
